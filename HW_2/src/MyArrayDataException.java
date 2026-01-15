1e98f96af7813b290b842d971fab0c922782d8a0
Konstantin1601 <57360460+Konstantin1601@users.noreply.github.com>
2026-01-15 19:38:18 +0800

    Delete HW_2 directory

--- HW_2/src/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-public class MyArrayDataException extends Exception {
-
-    public MyArrayDataException(String msg, int i, int j){
-        super(msg);
-        System.out.println("Строка: " + i + ", столбец: " + j);
-    }
-}
